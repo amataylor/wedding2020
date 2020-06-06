b59d599d44bde4f0f34513633a10c2b42ccd8486
Adam Taylor <adam.mark.taylor@gmail.com>
2020-06-06 16:39:25 +0100

    .
    .

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+  new Pageable("#container", {
+    animation: 500,
+    delay: 400,
+    pips: true,
+    orientation: "vertical",
+    onScroll: function() {
+      // do something during scroll
+    },
+    // many more
+  });
